d651a724c15e8ebffc4b530e175ce882406e8a29
kaisei <klay.11.kc@gmail.com>
2022-05-15 19:13:39 +0900

    2022 05 15 update

--- get_hour_data.py
+++ get_hour_data.py
@@ -3,25 +3,31 @@ from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
-# get exe file path
-driver =   webdriver.Chrome(ChromeDriverManager().install())
-# get url
-driver.get('https://sec.himawari-group.co.jp/report/chart/')
-# choice interval
-dropdown = driver.find_element_by_name('candle_term')
-select = Select(dropdown)
-select.select_by_value('_h')
-sleep(4)
-# choice currency pair
-dropdown = driver.find_element_by_name('currency_pair')
-select = Select(dropdown)
-list_curency = ['USDJPY','EURJPY','GBPJPY','AUDJPY','NZDJPY','CADJPY','CHFJPY','AUDUSD','EURUSD','GBPUSD','NZDUSD']
-for i in list_curency:
-    select.select_by_value(i)
-    # click keep button
-    btn = driver.find_element_by_css_selector('.csv_download.btn.btn-lg.center-block')
-    btn.click()
-    # sleep 8seconds
-    sleep(8)
-# quit system
-driver.quit()
+
+def get_hour_data():
+    
+    # get exe file path
+    driver =   webdriver.Chrome(ChromeDriverManager().install())
+    # get url
+    driver.get('https://sec.himawari-group.co.jp/report/chart/')
+    # choice interval
+    dropdown = driver.find_element_by_name('candle_term')
+    select = Select(dropdown)
+    select.select_by_value('_h')
+    sleep(4)
+    # choice currency pair
+    dropdown = driver.find_element_by_name('currency_pair')
+    select = Select(dropdown)
+    list_curency = ['USDJPY','EURJPY','GBPJPY','AUDJPY','NZDJPY','CADJPY','CHFJPY','AUDUSD','EURUSD','GBPUSD','NZDUSD']
+    for i in list_curency:
+        select.select_by_value(i)
+        # click keep button
+        btn = driver.find_element_by_css_selector('.csv_download.btn.btn-lg.center-block')
+        btn.click()
+        # sleep 8seconds
+        sleep(8)
+    # quit system
+    driver.quit()
+
+if __name__=='__main__':
+    get_hour_data()
